22792e13121cd08d3e04b1290a08d009756ed4d1
Diego Ribas González <diegoribas59@gmail.com>
2023-10-03 09:59:07 +0200

    Update

--- RibasGonzalez/main.py
+++ RibasGonzalez/main.py
@@ -5,16 +5,15 @@ import sys, var
 class Main (QtWidgets.QMainWindow):
     def __init__(self):
         super(Main, self).__init__()
-        var.ui = Ui_WindowMain()
+        var.ui = Ui_MainWindow()
         var.ui.setupUi(self) #encargado la interfaz
 
         """
         zona de eventos
         """
-        var.ui.btnSalir.clicked.connect(eventos.Eventos.saludar)
 
 if __name__ == '__main__':
         app = QtWidgets.QApplication([])
         window = Main()
-        window.show()
+        window.showMaximized()
         sys.exit(app.exec())
