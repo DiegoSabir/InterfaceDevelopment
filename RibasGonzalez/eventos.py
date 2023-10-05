3e31674d541526479f72134fc7974444beb84d40
Diego Ribas González <diegoribas59@gmail.com>
2023-10-05 14:21:09 +0200

    update

--- RibasGonzalez/eventos.py
+++ RibasGonzalez/eventos.py
@@ -1,8 +1,22 @@
-import var
+import var, sys
 
 class Eventos():
-    def saludar(self):
+    def salir(self):
         try:
-            var.ui.lblTitulo.setText("Hola has usado el boton")
+            sys.exit(0)
         except Exception as error:
-            print(error, "es modulo eventos")
+            print(error, "en modulo eventos")
+
+    @staticmethod
+    def abrirCalendar(self):
+        try:
+            var.calendar.show()
+        except Exception as error:
+            print("error al abrir calendar", error)
+
+    @staticmethod
+    def acercade():
+        try:
+            pass
+        except Exception as error:
+            print("error al abrir acercade", error)
--- RibasGonzalez/main.py
+++ RibasGonzalez/main.py
@@ -1,16 +1,33 @@
-import eventos
+import sys, var, eventos
 from mainwindow import *
-import sys, var
+from calendar import *
+from datetime import datetime
+
+class Calendar(QtWidgets.QDialog):
+    def __init__(self):
+        super(Calendar, self).__init__()
+        var.calendar = Ui_dlgCalendar()
+        var.calendar.setupUi(self)
+        dia = datetime.now().day
+        mes = datetime.now().month
+        year = datetime.now().year
 
 class Main (QtWidgets.QMainWindow):
     def __init__(self):
         super(Main, self).__init__()
         var.ui = Ui_MainWindow()
         var.ui.setupUi(self) #encargado la interfaz
+        var.calendar = Calendar()
 
         """
         zona de eventos
         """
+        var.ui.btnCalendar.clicked.connect(eventos.Eventos.abrirCalendar)
+
+        """
+        zona de eventos del menubar
+        """
+        var.ui.actionSalir.triggered.connect(eventos.Eventos.salir)
 
 if __name__ == '__main__':
         app = QtWidgets.QApplication([])
